15751510205516787497c4a5c7e1f48b31701678
mohamed medhat <73649080+CoDeBrEaKe@users.noreply.github.com>
2023-10-14 15:08:33 +0300

    Social links added

--- src/App.js
+++ src/App.js
@@ -1,27 +1,39 @@
-import './App.css';
-import React, {useState} from 'react';
+import "./App.css";
+import React, { useState } from "react";
 
+import {
+  FacebookIcon,
+  TelegramShareButton,
+  FacebookShareButton,
+  TelegramIcon,
+  WhatsappShareButton,
+  LinkedinIcon,
+  WhatsappIcon,
+  LinkedinShareButton,
+} from "react-share";
 const App = () => {
   const url = "https://api.quotable.io/random";
   let quoteData = {
     content: "Let time be your only competitor.",
-    author: "Ahmed Saber"
-  }
-  const [quote, setQuote] = useState(quoteData)
+    author: "Ahmed Saber",
+  };
+  const [quote, setQuote] = useState(quoteData);
 
   const generateQuote = () => {
     fetch(url)
-      .then(response => response.json())
-      .then(data => {
-        console.log(data)
-        setQuote(data)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setQuote(data);
       });
-  }
+  };
 
   const copy = () => {
-    navigator.clipboard.writeText(quote.author + " once said: " + quote.content)
-    alert('copied')
-  }
+    navigator.clipboard.writeText(
+      quote.author + " once said: " + quote.content
+    );
+    alert("copied");
+  };
 
   return (
     <>
@@ -30,13 +42,32 @@ const App = () => {
         <p>{quote.content}</p>
         <span>{quote.author}</span>
         <div className="btns">
-          <button onClick={copy} className="btn">Copy</button>
+          <button onClick={copy} className="btn">
+            Copy
+          </button>
           <button onClick={generateQuote}>Generate Another Quote</button>
+        </div>
+        <div className="share">
+          <FacebookShareButton url="sdsdq" quote={quoteData.content}>
+            <FacebookIcon quote={quoteData.content} />
+          </FacebookShareButton>
+          <TelegramShareButton title={quoteData.content} url={"/"}>
+            <TelegramIcon size={50} round={true} />
+          </TelegramShareButton>
+          <WhatsappShareButton
+            title={quoteData.content}
+            separator={" "}
+            url={"/"}
+          >
+            <WhatsappIcon size={50} round={true} />
+          </WhatsappShareButton>
+          <LinkedinShareButton url={"/"}>
+            <LinkedinIcon />
+          </LinkedinShareButton>
         </div>
       </div>
     </>
-  )
-}
-
+  );
+};
 
 export default App;
